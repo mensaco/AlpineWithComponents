d8193abf9ca038e9722558d272eed388117e044a
Mensur Islami <mi@live.de>
2024-11-17 23:03:48 +0100

    Update

--- Scripts/index.js
+++ Scripts/index.js
@@ -5,33 +5,46 @@ window.App = () => {
         testtext: 'test text',
         showHide: false,
 
-        componentsNames: [
-            'labelled-text'
-        ],
+       
         _componentDefinitions: {},
-        init() {
-            this.componentsNames.forEach(tag => {
+        /// read component definitions into the memory
+        async getComponents() {
+            const r = await fetch(`/Components/Components.html`)
+            const htmlText = await r.text()
+            const frag = document.createElement('div')
+            frag.innerHTML = htmlText;
+            [...frag.children].forEach(child => {
+                console.log(child.localName)
+                this._componentDefinitions[child.localName] = child.innerHTML
+            });
+            delete frag
+            delete htmlText
+            delete r
+        },
+        async init() {
+
+            await this.getComponents()            
+
+            Object.keys(this._componentDefinitions).forEach(tag => {
                 const components = [...document.querySelectorAll(tag)];
                 components.forEach(async component => {
                     if (this._componentDefinitions[tag] == undefined) {
-                        const r = await fetch(`/Components/${tag}.html`)
-                        const htmlText = await r.text()
+                        alert(`A ${tag} definition was not found`)
+                    }
+                    else {
+                        let replacement = this._componentDefinitions[tag];
 
-                        this._componentDefinitions[tag] = htmlText
-                        delete htmlText
-                        delete r
+                        [...component.attributes].forEach(a => {
+                            replacement = replacement.replace(a.name, a.value)
+                        })
+
+                        const creator = document.createElement("div")
+                        creator.innerHTML = replacement.trim()
+                        component.parentNode.replaceChild(creator.firstElementChild, component)
+                        delete creator
                     }
 
-                    let replacement = this._componentDefinitions[tag];
 
-                    [...component.attributes].forEach(a => {
-                        replacement = replacement.replace(a.name, a.value)
-                    })
-
-                    const creator = document.createElement("div")
-                    creator.innerHTML = replacement
-                    component.parentNode.replaceChild(creator.firstElementChild, component)
-                    delete creator
                 });
 
             });
